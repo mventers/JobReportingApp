a558d087dbf8acdac9390801fa34f2265a74de30
mventers <scottymventerprises@gmail.com>
2013-10-22 12:24:55 +1030

    initial commit

---
+++ JobReportingApp/Controllers/JobsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JobReportingApp.Models;
+
+namespace JobReportingApp.Controllers
+{
+    public class JobsController : Controller
+    {
+        private JobDBContext db = new JobDBContext();
+
+        //
+        // GET: /Jobs/
+
+        public ActionResult Index()
+        {
+            return View(db.Jobs.ToList());
+        }
+
+        //
+        // GET: /Jobs/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Job job = db.Jobs.Find(id);
+            if (job == null)
+            {
+                return HttpNotFound();
+            }
+            return View(job);
+        }
+
+        //
+        // GET: /Jobs/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Jobs/Create
+
+        [HttpPost]
+        public ActionResult Create(Job job)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Jobs.Add(job);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(job);
+        }
+
+        //
+        // GET: /Jobs/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Job job = db.Jobs.Find(id);
+            if (job == null)
+            {
+                return HttpNotFound();
+            }
+            return View(job);
+        }
+
+        //
+        // POST: /Jobs/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Job job)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(job).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(job);
+        }
+
+        //
+        // GET: /Jobs/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Job job = db.Jobs.Find(id);
+            if (job == null)
+            {
+                return HttpNotFound();
+            }
+            return View(job);
+        }
+
+        //
+        // POST: /Jobs/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Job job = db.Jobs.Find(id);
+            db.Jobs.Remove(job);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        public ActionResult SearchIndex(string searchString)
+        {
+            var jobs = from m in db.Jobs
+                       select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                jobs = jobs.Where(s => s.JobNumber.Contains(searchString));
+            }
+
+            return View(jobs);
+        }
+    }
+}
---
+++ JobReportingApp/Models/Job.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace JobReportingApp.Models
+{
+    public class Job
+    {
+        public int ID { get; set; }
+        public string JobNumber { get; set; }
+        public string AttendDate { get; set; }
+        public string AttendingTech { get; set; }
+        public string StatusNow { get; set; }
+        public string OdModel { get; set; }
+        public string OdSerial { get; set; }
+        public string IdModel { get; set; }
+        public string IdSerial { get; set; }
+        public string POP { get; set; }
+        public string PurchInv { get; set; }
+        public DateTime DOP { get; set; }
+        public string Report { get; set; }
+        public string StatusAfter { get; set; }
+        public string Visits { get; set; }
+        public decimal Labour { get; set; }
+        public string SentEmail { get; set; }
+    }
+
+    public class JobDBContext : DbContext
+    {
+        public DbSet<Job> Jobs { get; set; }
+    }
+}
